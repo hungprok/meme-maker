3baf4458d033baca89d74def770272049eae3e54
hungprok <57722641+hungprok@users.noreply.github.com>
2020-04-16 01:50:49 +0700

    add modal

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,45 @@
 var express = require('express');
 var router = express.Router();
 const upload = require('../utils/upload');
-const {loadData,  saveData} = require('../utils/data')
+const { loadData, saveData } = require('../utils/data');
+// const modal = require('modal');
+
+
+onclick = function () {
+  // modal.style.display = "block";
+  console.log('Modal clicked')
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/upload', upload.single('fileUpload'), function(req, res,) {
+router.post('/upload', upload.single('fileUpload'), function (req, res, ) {
   console.log(req.file)
 
-if (!req.file){
-  res.render('allImages', {error: 'You need to choose a file to upload'})
-}
+  if (!req.file) {
+    res.render('allImages', { error: 'You need to choose a file to upload' })
+  }
 
-const data = loadData()
-data.push(req.file)
-saveData(data)
+  const data = loadData()
+  data.push(req.file)
+  saveData(data)
 
-  res.render('allImages', {data})
+  res.render('allImages', { data, onclick })
 })
 
-router.get('/browse', upload.single('fileUpload'), function(req, res,) {
+router.get('/browse', upload.single('fileUpload'), function (req, res, ) {
   const data = loadData()
-  
-    res.render('allImages', {data})})
-module.exports = router;
 
-//
+  res.render('allImages', { data })
+})
+
+router.post('/view', function (req, res, ) {
+  console.log(req)
+  if (req.body) {
+    onclick();
+  }
+
+})
+module.exports = router;
