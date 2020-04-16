6763e139f97efb0b49b28ac8105e1dce6efe7600
hungprok <57722641+hungprok@users.noreply.github.com>
2020-04-17 02:50:50 +0700

    final

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const upload = require('../utils/upload');
-const {loadData,saveData, loadMeme, saveMeme} = require('../utils/data');
+const { loadData, saveData, loadMeme, saveMeme } = require('../utils/data');
 // const modal = require('modal');
 const jimp = require('jimp');
 var bodyParser = require('body-parser')
@@ -23,18 +23,30 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/upload', upload.single('fileUpload'), function (req, res, ) {
+router.post('/upload', upload.single('fileUpload'), async (req, res, ) => {
 
   if (!req.file) {
     res.render('index', { error: 'You need to choose a file to upload' });
     return;
   }
-  console.log(req.file)
+  const image = await jimp.read('public/uploads/' + req.file.originalname);
+  image.resize(jimp.AUTO, 300, jimp.RESIZE_BEZIER);
+  const newfilename = 'resize-' + req.file.originalname;
+  image.write('public/uploads/' + newfilename);
 
-  const data = loadData()
-  data.push(req.file)
+  const data = loadData();
+
+  var latestid;
+  if (data.length == 0) {
+    latestid = 1
+  } else { latestid = data[data.length - 1].ID + 1 }
+
+  const newData = req.file;
+  newData['newname'] = newfilename;
+  newData['ID'] = latestid;
+  console.log(req.file.originalname)
+  data.push(newData)
   saveData(data)
-
   res.render('allImages', { data })
 })
 
@@ -47,7 +59,7 @@ router.get('/browse', upload.single('fileUpload'), function (req, res, ) {
 
 router.get('/meme', function (req, res, ) {
   const data = loadMeme();
-  console.log('this is from meme tab',data);
+  console.log('this is from meme tab', data);
 
   console.log(data);
   res.render('meme', { data })
@@ -70,14 +82,36 @@ router.post('/preview', urlencodedParser, async (req, res) => {
   const filename = req.body.imageInfo;
   const image = await jimp.read('public/uploads/' + filename);
   const font = await jimp.loadFont(jimp.FONT_SANS_32_WHITE);
-  image.resize(jimp.AUTO, 300, jimp.RESIZE_BEZIER);
-  image.print(font, 0, 10, req.body.top);
-  image.print(font, 0, 250, req.body.bottom)
-  const newfilename = 'jimp-' + Date.now() +'-' + filename;
+  // image.print(font, 0, 10, req.body.top);
+  // image.print(font, 0, 210, req.body.bottom);
+
+  var w = image.bitmap.width;
+  var h = image.bitmap.height;
+  console.log(w,h)
+  let text = "example"
+  var textWidth = jimp.measureText(font, text);
+  var textHeight = jimp.measureTextHeight(font, text);
+  image.print(
+    font, w/2 - textWidth/2, 5 - textHeight/2,
+        {   
+        text: req.body.top,
+        alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,                                      
+        }, textWidth, textHight
+  );
+
+  image.print(
+    font, w/2 - textWidth/2, h-75 - textHeight/2,
+        {   
+        text: req.body.bottom,
+        alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,                                      
+        }, textWidth, textHight
+  );
+
+  const newfilename = 'jimp-' + Date.now() + '-' + filename;
   image.write('public/uploads/' + newfilename);
 
   const data = loadMeme()
-  data.push({newfilename})
+  data.push({ newfilename })
   saveMeme(data)
 
   res.render('preview', { newfilename })
